6f605ef339f3abd4397665204fcb3c3dad4c5422
nospoonvue <nospoonvue@nospoon.nl>
2020-11-13 00:09:32 +0100

    hjk'

---
+++ src/views/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contact">
+    <h1>Contact</h1>
+    <p>
+      Starbase18<br/> 
+       Blokhuisplein 40, <br/>
+       Leeuwarden, <br/>
+       Nederland<br/>
+       +31-6-25426731<br/>
+      f.kroondijk@fcroc.nl<br/>
+
+
+      </p>
+  </div>
+</template>
