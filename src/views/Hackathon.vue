295a20c421a3e68cc063db902889a2cc2752d560
nospoonvue <nospoonvue@nospoon.nl>
2021-02-05 12:33:22 +0100

    fs

---
+++ src/views/Hackathon.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+	<!-- Banner -->
+		<section id="banner" class="banner">
+			<div class="content">
+				<p class="headerimg" style="width:100%;text-allign:center"><img src="images/sb18hackatonBanner.jpg"/></p>
+				<header>											
+					<h1>Hackathon during the Week of the circular economy</h1>
+					<p>Hybrid event</p>
+				</header>	
+				<h2>Design a
+circular mini-village
+for EU students 
+in central Portugal</h2>									
+				<p>
+				The current school system is based on the linear 
+economy, to prepare students for the circular economy 
+a whole new way of teaching is required. One way of 
+doing so is by studying and working in real-life living labs. 
+So a property with 5 hectares of land has been purchased.
+
+				</p>	
+				<p>
+					Living labs, as a serious playground, in which students, companies, institutes and governments can jointly work on finding solutions for complex circular challenges. How do we solve soil degradation, water management, social inclusion, finite resources, decreasing biodiversity, an aging population 
+and brain drain to the cities, in short:
+ how do we solve the big questions of today and tomorrow?
+In our living lab we want to invite students from all over the EU to jointly
+work on these big questions. We have set the stage, but
+how should this serious playground in Portugal look like?<br/><br/>
+That’s the question that you are going to work on:<br/></br>
+step 1 - get familiar with the concept<br/>
+step 2 - what concrete things should be available and why<br/>
+step 3 - what is most urgent and important<br/>
+step 4 - Visualize in a poster and 3D model<br/>
+					</p>	
+					<p>
+						sign-up now by sending an e-mail to: f.kroondijk@fcroc.nl
+						</p>
+			</div>
+  		</section>
+  </div>  
+</template>
+
+<script>
+
+</script>
+<style>
+
+@media screen and (max-width: 900px) {
+    iframe {
+        max-width: 100% ;
+        width: 100% ;
+        height: auto ;
+    }
+}
+
+</style>
