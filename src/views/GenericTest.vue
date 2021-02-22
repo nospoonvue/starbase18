89de948a50e7ef538caeca4c080716b6eed6bcdd
nospoonvue <nospoonvue@nospoon.nl>
2021-02-22 20:18:08 +0100

    gd

---
+++ src/views/GenericTest.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="blog">    
+        <GenericPage class="" :page="page" />
+    </div>
+</template>
+
+
+<style >
+
+</style>
+
+<script>
+
+import GenericPage from '@/components/GenericPage.vue'
+
+export default 
+{
+    name: "genericPage",
+    components: {GenericPage},
+
+    data() 
+    {
+        return {
+        page: "Home"
+        }
+    },
+    methods:{
+
+    },
+    computed: {
+
+    },
+    created(){
+
+    },
+}
+
+
+</script>
+
+
