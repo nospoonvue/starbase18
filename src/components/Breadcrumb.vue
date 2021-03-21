16e024443a2840825964c5f893482d40f5e7fcc4
nospoonvue <nospoonvue@nospoon.nl>
2021-03-21 01:47:42 +0100

    fs

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,51 @@
+<template>
+ <div id="breadcrumb" style="clear:both;">     
+     <span v-for="(item) in pages" :key="item.id">
+         / <a class="breadcrumb" :href="'/#/'+item.url">{{item.title}}</a>
+     </span>
+</div>
+</template>
+
+<style >
+#breadcrumb
+{
+    padding-top: 10px;
+}
+.breadcrumb
+{
+    font-size: small;
+}
+</style>
+
+<script>
+export default 
+{
+    name: "Breadcrumb",
+    components: {},
+
+    data() 
+    {
+    return {
+
+        }
+    },
+    props: {
+        pages: null
+    },
+    created() 
+    {
+
+    },
+    methods:
+    {
+  
+    },
+    computed: {
+ 
+  },
+}
+
+
+</script>
+
+
