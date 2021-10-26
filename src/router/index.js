dd1dfaee36ddcbc9362b26079f84889083d5234c
nospoonvue <nospoonvue@nospoon.nl>
2021-10-26 02:04:11 +0100

    jhk

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import Mentors from '../views/Mentors.vue'
 import Join from '../views/Join.vue'
 import Merchandise from '../views/Merchandise.vue'
 import Meta from 'vue-meta'
+import SoCeCo from '../views/SoCeCo.vue'
 
 Vue.use(Meta)
 
@@ -38,6 +39,11 @@ Vue.use(VueRouter)
     name: 'Contact',
     component: Contact
     },
+    {
+      path: '/soceco',
+      name: 'SoCeCo',
+      component: SoCeCo
+      }
     ,
   {
   path: '/projectTypes',
---
+++ src/views/SoCeCo.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="SoCeCo">    
+        <GenericPage class="notranslate" :page="page" :paging="paging" :pageSize="pageSize"/>
+    </div>
+</template>
+
+<style >
+</style>
+
+<script>
+import GenericPage from '@/components/GenericPage.vue'
+
+export default 
+{
+    name: "genericPage",
+    components: {GenericPage},
+
+    data() 
+    {
+        return {
+        page: "SoCeCo",
+        paging: false,
+        pageSize: 2
+        }
+    },
+    metaInfo() {
+        return {
+            title: 'Starbase18 - ' + this.page,
+            meta:[
+                {
+                    name: "description",
+                    content: "Join Starbase18, contact us and tell us how you would like to contribute"
+                },
+                {
+                    name: "keywords",
+                    content: "Contacting, sponsoring, helping, machines, materials"
+                },
+                {
+                    name: "author",
+                    content: "Frank Kroondijk"
+                },
+            ]
+      }
+    }
+}
+
+
+</script>
+
+
